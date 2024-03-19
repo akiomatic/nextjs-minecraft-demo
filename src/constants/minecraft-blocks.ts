e75a3c382b45f68d2a549a0e04b5290f712da144
akio <124953279+akiomatic108@users.noreply.github.com>
2024-03-18 23:48:35 -0700

    feat: add Minecraft blocks constant

---
+++ src/constants/minecraft-blocks.ts
@@ -0,0 +1,12 @@
+export enum BlockType {
+  AIR = "AIR",
+  DIRT = "DIRT",
+  GRASS = "GRASS",
+  STONE = "STONE",
+  LEAVES = "LEAVES",
+  WOOD = "WOOD",
+  DEEPSLATE = "DEEPSLATE",
+  COAL_ORE = "COAL_ORE",
+  IRON_ORE = "IRON_ORE",
+  DIAMOND_ORE = "DIAMOND_ORE",
+}
